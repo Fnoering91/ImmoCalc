ddaacf312512e1fea9e291fda00d454b26a2413e
Fnoering91 <noering.fabian@gmail.com>
2025-04-16 18:01:18 +0200

    Update inputs.py
    keys und session state ergänzt

--- modules/inputs.py
+++ modules/inputs.py
@@ -8,39 +8,39 @@ def eingabeformular():
         col1, col2 = st.columns(2)
 
         with col1:
-            kaufpreis = st.number_input("Kaufpreis (€)", min_value=10000, value=316000, step=10000, help="= Kaufpreis OHNE Kaufnebenkosten")
-            eigenkapital = st.number_input("Eigenkapital (€)", min_value=0, value=30000, step=10000, help="= Eigenkapital, welches den notwendigen Kreditrahmen reduziert")
-            zinssatz = st.number_input("Zinssatz (p.a.)", min_value=0.1, max_value=10.0, value=3.8, step=0.1, help="= Zinssatz für Kredit")
-            laufzeit_jahre = st.number_input("Laufzeit (Jahre)", min_value=5, max_value=40, value=25, help="= Laufzeit des Kredits")
-            nebenkosten_kauf = st.number_input("Kaufnebenkosten (%)", min_value=0.0, max_value=20.0, value=7.0, help="= Grunderwerbsteuer, Makler, Notar etc.")                
+            kaufpreis = st.number_input("Kaufpreis (€)", min_value=10000, value=st.session_state.get("kaufpreis", 316000), step=10000, help="= Kaufpreis OHNE Kaufnebenkosten", key="kaufpreis")
+            eigenkapital = st.number_input("Eigenkapital (€)", min_value=0, value=st.session_state.get("eigenkapital", 30000), step=10000, help="= Eigenkapital, welches den notwendigen Kreditrahmen reduziert", key="eigenkapital")
+            zinssatz = st.number_input("Zinssatz (p.a.)", min_value=0.1, max_value=10.0, value=st.session_state.get("zinssatz", 3.8), step=0.1, help="= Zinssatz für Kredit", key="zinssatz")
+            laufzeit_jahre = st.number_input("Laufzeit (Jahre)", min_value=5, max_value=40, value=st.session_state.get("laufzeit_jahre", 25), help="= Laufzeit des Kredits", key="laufzeit_jahre")
+            nebenkosten_kauf = st.number_input("Kaufnebenkosten (%)", min_value=0.0, max_value=20.0, value=st.session_state.get("nebenkosten_kauf", 7.0), help="= Grunderwerbsteuer, Makler, Notar etc.", key="nebenkosten_kauf")                
         
         with col2:            
-            wohnfläche = st.number_input("Wohnfläche (m²)", min_value=10, value=56, help="= zur Berechnung von Mieteinnahmen")
-            kaltmiete = st.number_input("Kaltmiete (€/m²)", min_value=1.0, value=16.0, step=0.1, help="= durchschnittlicher Mietpreis")
-            mieterhoehung = st.slider("Jährliche Mieterhöhung (%)", 0.0, 5.0, value=1.0, step=0.1)
-            region = st.text_input("Region der Immobilie", value="Hamburg", help="= Stadt oder Ballungsraum. Wird nur für die Expertenmeinung verwendet.")
-            stadtteil = st.text_input("Stadtteil der Immobilie", value = "Bergedorf", help="Wird nur für die Expertenmeinung verwendet.")
+            wohnfläche = st.number_input("Wohnfläche (m²)", min_value=10, value=st.session_state.get("wohnfläche", 56), help="= zur Berechnung von Mieteinnahmen", key="wohnfläche")
+            kaltmiete = st.number_input("Kaltmiete (€/m²)", min_value=1.0, value=st.session_state.get("kaltmiete", 16.0), step=0.1, help="= durchschnittlicher Mietpreis", key="kaltmiete")
+            mieterhoehung = st.slider("Jährliche Mieterhöhung (%)", 0.0, 5.0, value=st.session_state.get("mieterhoehung", 1.0), step=0.1, key="mieterhoehung")
+            region = st.text_input("Region der Immobilie", value=st.session_state.get("region", "Hamburg"), help="= Stadt oder Ballungsraum. Wird nur für die Expertenmeinung verwendet.", key="region")
+            stadtteil = st.text_input("Stadtteil der Immobilie", value = st.session_state.get("stadtteil", "Bergedorf"), help="Wird nur für die Expertenmeinung verwendet.", key="stadtteil")
 
         st.markdown("---")
         col21, col22 = st.columns(2)
         with col21:
-            steuersatz = st.slider("Persönlicher Steuersatz (%)", 0, 50, value=42)
-            nicht_umlagefaehige_kosten = st.number_input("Nicht umlagefähige Nebenkosten (€/m² p.a.)", min_value=0.0, value=25.0, step=1.0, help = "Diese fallen regelmäßig an und können nicht auf den Mieter umgelegt werden (Höhe typischerweise ca. 20–30 €/m²/Jahr). Dazu zählen u.a. Verwaltungskosten (z. B. Hausverwaltung), Instandhaltungsrücklage, Reparaturkosten, Bankgebühren, ggf. Leerstandskosten (z. B. Mietausfallversicherung)")            
+            steuersatz = st.slider("Persönlicher Steuersatz (%)", 0, 50, value=int(st.session_state.get("steuersatz", 0.42) * 100), key="steuersatz")
+            nicht_umlagefaehige_kosten = st.number_input("Nicht umlagefähige Nebenkosten (€/m² p.a.)", min_value=0.0, value=st.session_state.get("nicht_umlagefaehige_kosten", 25.0), step=1.0, help = "Diese fallen regelmäßig an und können nicht auf den Mieter umgelegt werden (Höhe typischerweise ca. 20–30 €/m²/Jahr). Dazu zählen u.a. Verwaltungskosten (z. B. Hausverwaltung), Instandhaltungsrücklage, Reparaturkosten, Bankgebühren, ggf. Leerstandskosten (z. B. Mietausfallversicherung)", key="nicht_umlagefaehige_kosten")            
         with col22:
-            annahme_wertsteigerung = st.slider("Wertsteigerung der Immobilie pro Jahr (%)", -5.0, 5.0, value=0.0, step=0.1, help="Nominal = OHNE Einbezug von Inflation. --> Nominal, also ohne Berücksichtigung der Inflation, stiegen die Immobilienpreise in Deutschland seit 1975 um etwa 215 %. Das entspricht einer durchschnittlichen jährlichen Steigerung von ungefähr 2,7 %. ")
-            annahme_inflation = st.slider("Inflation pro Jahr (%)", -5.0, 5.0, value=1.0, step=0.1, help="")                
+            annahme_wertsteigerung = st.slider("Wertsteigerung der Immobilie pro Jahr (%)", -5.0, 5.0, value=st.session_state.get("annahme_wertsteigerung", 0.0), step=0.1, help="Nominal = OHNE Einbezug von Inflation. --> Nominal, also ohne Berücksichtigung der Inflation, stiegen die Immobilienpreise in Deutschland seit 1975 um etwa 215 %. Das entspricht einer durchschnittlichen jährlichen Steigerung von ungefähr 2,7 %. ", key="annahme_wertsteigerung")
+            annahme_inflation = st.slider("Inflation pro Jahr (%)", -5.0, 5.0, value=st.session_state.get("annahme_inflation", 1.0), step=0.1, help="", key="annahme_inflation")                
 
         st.markdown("---")
         col31, col32 = st.columns(2)
         with col31:
-            exit_aktiv = st.checkbox("Exit Option berechnen", value=False, help="Exit bezeichnet die Möglichkeit frühzeitig aus dem Kredit auszusteigen, indem die Immobilie verkauft wird und der steuerfreie Gewinn durch Wertsteigerung realisiert wird.")
+            exit_aktiv = st.checkbox("Exit Option berechnen", value=st.session_state.get("exit_aktiv", False), help="Exit bezeichnet die Möglichkeit frühzeitig aus dem Kredit auszusteigen, indem die Immobilie verkauft wird und der steuerfreie Gewinn durch Wertsteigerung realisiert wird.", key="exit_aktiv")
         with col32:
-            exit_nach = st.number_input("Exit nach (Jahre)", min_value=10, max_value=30, value=10, help=" nach 10 Jahren Haltedauer sind die Gewinne durch Verkauf steuerfrei!")
+            exit_nach = st.number_input("Exit nach (Jahre)", min_value=10, max_value=30, value=st.session_state.get("exit_nach", 10), help=" nach 10 Jahren Haltedauer sind die Gewinne durch Verkauf steuerfrei!", key="exit_nach")
         
         st.markdown("---")
         col41, col42 = st.columns(2)
         with col41:
-            experteneinschaetzung_aktiv = st.checkbox("GPT-Experteneinschätzung aktivieren", value=False)
+            experteneinschaetzung_aktiv = st.checkbox("GPT-Experteneinschätzung aktivieren", value=st.session_state.get("experteneinschaetzung_aktiv", False), key="experteneinschaetzung_aktiv")
         
         submitted = st.form_submit_button("Finanzierung berechnen")
     return submitted, {
